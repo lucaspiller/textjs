dfb348ab619892bfc858eca76241c889eb6bc0ec
Luca Spiller <luca@stackednotion.com>
2011-07-05 20:33:08 +0100

    Move SmsColumns into MessagesAdapter

--- src/org/stackednotion/httpserver/adapters/MessagesAdapter.java
+++ src/org/stackednotion/httpserver/adapters/MessagesAdapter.java
@@ -14,10 +14,10 @@ import android.net.Uri;
 // for reading SMS messages
 // based upon: http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;f=core/java/android/provider/Telephony.java;h=bf9e8549aaaf406230cff15346eb1ff2add862e1;hb=HEAD
 public class MessagesAdapter {
-	public static final String[] MESSAGES_PROJECTION = { SmsColumns.ID,
-			SmsColumns.TYPE, SmsColumns.THREAD_ID, SmsColumns.ADDRESS,
-			SmsColumns.DATE, SmsColumns.READ, SmsColumns.STATUS,
-			SmsColumns.BODY };
+	public static final String[] MESSAGES_PROJECTION = { MessageColumns.ID,
+			MessageColumns.TYPE, MessageColumns.THREAD_ID,
+			MessageColumns.ADDRESS, MessageColumns.DATE, MessageColumns.READ,
+			MessageColumns.STATUS, MessageColumns.BODY };
 
 	public static Collection<Message> all(Integer page, Integer limit) {
 		Cursor cursor = Settings
@@ -50,7 +50,7 @@ public class MessagesAdapter {
 						"thread_id=" + Integer.toString(threadId), null, null);
 
 		ArrayList<Message> messages = new ArrayList<Message>();
-		
+
 		cursor.moveToFirst();
 		while (cursor.moveToNext()) {
 			Message c = createMessageFromCursor(cursor);
@@ -64,27 +64,31 @@ public class MessagesAdapter {
 	public static Message createMessageFromCursor(Cursor cursor) {
 		Message message = new Message();
 
-		message.id = cursor.getInt(cursor.getColumnIndex(SmsColumns.ID));
+		message.id = cursor.getInt(cursor.getColumnIndex(MessageColumns.ID));
 
-		message.type = cursor.getInt(cursor.getColumnIndex(SmsColumns.TYPE));
+		message.type = cursor
+				.getInt(cursor.getColumnIndex(MessageColumns.TYPE));
 
 		message.thread_id = cursor.getInt(cursor
-				.getColumnIndex(SmsColumns.THREAD_ID));
+				.getColumnIndex(MessageColumns.THREAD_ID));
 
 		message.address = cursor.getString(cursor
-				.getColumnIndex(SmsColumns.ADDRESS));
+				.getColumnIndex(MessageColumns.ADDRESS));
 
 		message.sender_key = ContactsAdapter
 				.find_key_from_phone_number(message.address);
 
-		message.date = cursor.getLong(cursor.getColumnIndex(SmsColumns.DATE));
+		message.date = cursor.getLong(cursor
+				.getColumnIndex(MessageColumns.DATE));
 
-		message.read = cursor.getInt(cursor.getColumnIndex(SmsColumns.READ));
+		message.read = cursor
+				.getInt(cursor.getColumnIndex(MessageColumns.READ));
 
-		message.status = cursor
-				.getInt(cursor.getColumnIndex(SmsColumns.STATUS));
+		message.status = cursor.getInt(cursor
+				.getColumnIndex(MessageColumns.STATUS));
 
-		message.body = cursor.getString(cursor.getColumnIndex(SmsColumns.BODY));
+		message.body = cursor.getString(cursor
+				.getColumnIndex(MessageColumns.BODY));
 
 		return message;
 	}
@@ -119,5 +123,157 @@ public class MessagesAdapter {
 				return null;
 			}
 		}
+	}
+
+	public interface MessageColumns {
+		/**
+		 * The type of the message
+		 * <P>
+		 * Type: INTEGER
+		 * </P>
+		 */
+		public static final String ID = "_id";
+
+		public static final String TYPE = "type";
+
+		public static final int MESSAGE_TYPE_ALL = 0;
+		public static final int MESSAGE_TYPE_INBOX = 1;
+		public static final int MESSAGE_TYPE_SENT = 2;
+		public static final int MESSAGE_TYPE_DRAFT = 3;
+		public static final int MESSAGE_TYPE_OUTBOX = 4;
+		public static final int MESSAGE_TYPE_FAILED = 5; // for failed outgoing
+		// messages
+		public static final int MESSAGE_TYPE_QUEUED = 6; // for messages to send
+		// later
+
+		/**
+		 * The thread ID of the message
+		 * <P>
+		 * Type: INTEGER
+		 * </P>
+		 */
+		public static final String THREAD_ID = "thread_id";
+
+		/**
+		 * The address of the other party
+		 * <P>
+		 * Type: TEXT
+		 * </P>
+		 */
+		public static final String ADDRESS = "address";
+
+		/**
+		 * The person ID of the sender
+		 * <P>
+		 * Type: INTEGER (long)
+		 * </P>
+		 */
+		public static final String PERSON_ID = "person";
+
+		/**
+		 * The date the message was sent
+		 * <P>
+		 * Type: INTEGER (long)
+		 * </P>
+		 */
+		public static final String DATE = "date";
+
+		/**
+		 * Has the message been read
+		 * <P>
+		 * Type: INTEGER (boolean)
+		 * </P>
+		 */
+		public static final String READ = "read";
+
+		/**
+		 * Indicates whether this message has been seen by the user. The "seen"
+		 * flag will be used to figure out whether we need to throw up a
+		 * statusbar notification or not.
+		 */
+		public static final String SEEN = "seen";
+
+		/**
+		 * The TP-Status value for the message, or -1 if no status has been
+		 * received
+		 */
+		public static final String STATUS = "status";
+
+		public static final int STATUS_NONE = -1;
+		public static final int STATUS_COMPLETE = 0;
+		public static final int STATUS_PENDING = 64;
+		public static final int STATUS_FAILED = 128;
+
+		/**
+		 * The subject of the message, if present
+		 * <P>
+		 * Type: TEXT
+		 * </P>
+		 */
+		public static final String SUBJECT = "subject";
+
+		/**
+		 * The body of the message
+		 * <P>
+		 * Type: TEXT
+		 * </P>
+		 */
+		public static final String BODY = "body";
+
+		/**
+		 * The id of the sender of the conversation, if present
+		 * <P>
+		 * Type: INTEGER (reference to item in content://contacts/people)
+		 * </P>
+		 */
+		public static final String PERSON = "person";
+
+		/**
+		 * The protocol identifier code
+		 * <P>
+		 * Type: INTEGER
+		 * </P>
+		 */
+		public static final String PROTOCOL = "protocol";
+
+		/**
+		 * Whether the <code>TP-Reply-Path</code> bit was set on this message
+		 * <P>
+		 * Type: BOOLEAN
+		 * </P>
+		 */
+		public static final String REPLY_PATH_PRESENT = "reply_path_present";
+
+		/**
+		 * The service center (SC) through which to send the message, if present
+		 * <P>
+		 * Type: TEXT
+		 * </P>
+		 */
+		public static final String SERVICE_CENTER = "service_center";
+
+		/**
+		 * Has the message been locked?
+		 * <P>
+		 * Type: INTEGER (boolean)
+		 * </P>
+		 */
+		public static final String LOCKED = "locked";
+
+		/**
+		 * Error code associated with sending or receiving this message
+		 * <P>
+		 * Type: INTEGER
+		 * </P>
+		 */
+		public static final String ERROR_CODE = "error_code";
+
+		/**
+		 * Meta data used externally.
+		 * <P>
+		 * Type: TEXT
+		 * </P>
+		 */
+		public static final String META_DATA = "meta_data";
 	}
 }
