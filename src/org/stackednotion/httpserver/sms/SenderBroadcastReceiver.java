bbc5d606e14221305b517e6dab107ba855bcec08
Luca Spiller <luca@stackednotion.com>
2011-07-20 22:07:03 +0100

    Dont try and requeue a failed message due to radio issues

--- src/org/stackednotion/httpserver/sms/SenderBroadcastReceiver.java
+++ src/org/stackednotion/httpserver/sms/SenderBroadcastReceiver.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Telephony.Sms;
-import android.telephony.SmsManager;
 import android.util.Log;
 
 public class SenderBroadcastReceiver extends BroadcastReceiver {
@@ -34,35 +33,20 @@ public class SenderBroadcastReceiver extends BroadcastReceiver {
 		boolean sendNextMsg = intent.getBooleanExtra(
 				EXTRA_MESSAGE_SENT_SEND_NEXT, false);
 
-		switch (resultCode) {
-		case Activity.RESULT_OK:
+		if (resultCode == Activity.RESULT_OK) {
 			sentMessageSuccess(context, uri, error);
-			if (sendNextMsg) {
-				sendNextMessage(context);
-			}
-			break;
-		case SmsManager.RESULT_ERROR_RADIO_OFF:
-		case SmsManager.RESULT_ERROR_NO_SERVICE:
-			sentMessageFailedRadio(context, uri, error);
-			// TODO handle radio being off
-			break;
-		default:
+		} else {
 			sentMessageFailedGeneric(context, uri, resultCode, error);
-			if (sendNextMsg) {
-				sendNextMessage(context);
-			}
-			break;
+		}
+		
+		if (sendNextMsg) {
+			sendNextMessage(context);
 		}
 	}
 
 	private void sentMessageSuccess(Context context, Uri uri, int error) {
 		Log.v("HttpServer", "Successfully sent: " + uri);
 		Sms.moveMessageToFolder(context, uri, Sms.MESSAGE_TYPE_SENT, error);
-	}
-
-	private void sentMessageFailedRadio(Context context, Uri uri, int error) {
-		Log.v("HttpServer", "Failed to sent: " + uri + " no service");
-		Sms.moveMessageToFolder(context, uri, Sms.MESSAGE_TYPE_QUEUED, error);
 	}
 
 	private void sentMessageFailedGeneric(Context context, Uri uri,
