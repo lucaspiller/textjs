2092b11a7e0beb3812a62d91f892ef74d4ded80d
Luca Spiller <luca@stackednotion.com>
2011-07-28 22:16:04 +0100

    Rename to SmsJs

--- src/org/stackednotion/httpserver/SmsJs.java
+++ src/org/stackednotion/httpserver/SmsJs.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class SmsJs extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
