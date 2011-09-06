616fbc2d2880240b05152dd84168a285d3546355
Luca Spiller <luca@stackednotion.com>
2011-09-06 20:34:53 +0100

    Support backend pagination

--- public/javascripts/patches.js
+++ public/javascripts/patches.js
@@ -2,11 +2,44 @@
   var __ajax = $.ajax;
   $.ajax = function(options) {
     options = options || {};
+
+    // set accesscode if defined
     if (Application != undefined) {
       options.beforeSend = function(xhr) {
         xhr.setRequestHeader('Xaccesscode', Application.AccessToken);
       };
     }
+
+    // pagination
+    if (!options.baseUrl) {
+      options.baseUrl = options.url;
+    }
+    var success = options.success;
+    options.success = function(resp, status, xhr) {
+      var nextPage = xhr.getResponseHeader('X-Next-Page');
+      if (nextPage) {
+        if (options.lastResp) {
+          options.lastResp = options.lastResp.concat(resp);
+        } else {
+          options.lastResp = resp;
+        }
+        if (resp.length == 0) {
+          // end of pagination
+          if (success) success(options.lastResp, status, xhr);
+        } else {
+          options.url = options.baseUrl + '/' + nextPage;
+          __ajax(options);
+        }
+      } else {
+        if (options.lastResp) {
+          options.lastResp = options.lastResp.concat(resp);
+        } else {
+          options.lastResp = resp;
+        }
+        if (success) success(options.lastResp, status, xhr);
+      }
+    };
+
     return __ajax(options);
   };
 })( jQuery );
