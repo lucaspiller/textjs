ce7f06222d72bba94bdf7930d4fe977fa975b464
Luca Spiller <luca@stackednotion.com>
2011-09-18 13:13:58 +0100

    Remove old code

--- src/org/stackednotion/textjs/SettingsActivity.java
+++ src/org/stackednotion/textjs/SettingsActivity.java
@@ -67,21 +67,4 @@ public class SettingsActivity extends PreferenceActivity {
 
 		super.onPause();
 	}
-
-	/*
-	 * @Override public void onResume() {
-	 * Settings.init(getApplicationContext());
-	 * 
-	 * //String ip = Network.getLocalIpAddress(); //String port = "8080"; //
-	 * TODO //String authCode = "1234";
-	 * //String.valueOf(Settings.getAccessCode());
-	 * 
-	 * //TextView ipText = (TextView) findViewById(R.id.ip_text);
-	 * //ipText.setText("http://" + ip + ":" + port + "/");
-	 * 
-	 * //TextView authText = (TextView) findViewById(R.id.auth_text);
-	 * //authText.setText(authCode);
-	 * 
-	 * super.onResume(); }
-	 */
 }
